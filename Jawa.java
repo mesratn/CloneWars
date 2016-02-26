a3d5173da54485118aa68be254f64f3eea9d4c2f
MistSF <kevin@faby.fr>
2016-02-26 11:07:57 +0100

    jawa update

--- Jawa.java
+++ Jawa.java
@@ -1,5 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package clone;
 
-public class Jawa {
-
+/**
+ *
+ * @author Kevin
+ */
+public class Jawa extends Personnage{
+    public Jawa() {
+		
+        super(200, 400, 400, 0, 400);
+    }
 }
